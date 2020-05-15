acba5a5d835adf5a8e862dd641773384ddbc427f
travism26 <travismartin266@gmail.com>
2020-05-14 21:03:39 -0300

    got google auth working with the buttons too

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -4,27 +4,52 @@ class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
 
   componentDidMount() {
+    // Good spot to place this??? maybe? maybe not?
+    const KEY = process.env.REACT_APP_GOOGLE_AUTH_KEY;
     window.gapi.load("client:auth2", () => {
       window.gapi.client
         .init({
           clientId:
-            "!!!CLIENTKEYHERE!!!",
+            KEY,
           scope: "email",
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>Not sure if we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="google icon"/>
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignInClick}>
+          <i className="google icon"/>
+          Sign In with Google
+        </button>
+      );
     }
   }
 
