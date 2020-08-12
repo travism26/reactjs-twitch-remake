1881c990083e2e6d9bc690e21483f9c57e422888
travism26 <travismartin266@gmail.com>
2020-08-11 23:22:00 -0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,6 @@ import reducers from "./reducers";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducers,
-
   composeEnhancers(applyMiddleware(reduxThunk))
 );
 
