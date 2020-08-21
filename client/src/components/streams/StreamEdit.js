d2c72ec505e441696ebb77e6766be1e2f13e2030
travism26 <travismartin266@gmail.com>
2020-08-21 09:46:55 -0300

    adding edit stream functionality NOTE DEFECT removes userid

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 // const StreamEdit = (props) => {
 //     console.log(props);
@@ -11,12 +13,31 @@ class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    };
+
     render() {
         console.log(this.props);
         if (!this.props.stream) {
             return <div>Loading...</div>;
         }
-        return <div>{this.props.stream.title}</div>;
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm
+                    // initialValues={{ title: 'edit me', description: 'SUP' }}
+                    // initialValues={this.props.stream}
+                    initialValues={_.pick(
+                        this.props.stream,
+                        'title',
+                        'description'
+                    )}
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        );
     }
 }
 
@@ -24,4 +45,6 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+    StreamEdit
+);
