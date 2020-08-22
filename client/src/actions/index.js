1599cfb18b2ffb56946771fc7cc22630898db281
travism26 <travismartin266@gmail.com>
2020-08-22 14:48:26 -0300

    finished delete functionality and proper navigation back after actions are executed

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -53,6 +53,7 @@ export const deleteStream = (id) => async (dispatch) => {
     await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
 };
 
 // object base approach using Key Interpolation
